d926d5c7a8f3f84f997ef89087935071171ea10a
jeremygm23 <jeremygm5@gmail.com>
2019-05-16 18:24:18 -0400

    update logic in app.js using jquery to access gameBoard
    Used jquery instead of old DOM to place Xs and Os on gameboard and
    to switch between players(Xs/Os)

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,7 +12,9 @@ $(() => {
 
 // store the game board in a variable and have an array of 9 empty spots representing tic tac toe move options
 let gameBoard = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-// create two players: player x and player o
+
+
+// create players: player = 0 is X and player = 1 is O
 
 let player = 0
 
@@ -29,57 +31,77 @@ const winners = [
   [0, 4, 8],
   [6, 4, 2]
 ]
+
+
+
 // way to access each cell
 const cells = $('.cell')
 
+  //startGame()
 // access reset button to refresh gameboard when clicked
 $('.reset-btn').on("click", function() {
-  startGame()
+    $('.cell').html(" ")
+  })
+
+
+
+
+$('.cell').on("click", function(event) {
+  $(event.target).html("O")
 })
 
-startGame()
-
-
+$('.cell').on("click", function(event) {
+  if(player === 0) {
+    $(event.target).html("O")
+    player = 1
+  } else if (player === 1) {
+       $(event.target).html("X")
+    player = 0
+   }
+ })
 
 // create a function for when the game starts, displaying an empty gameboard.
-function startGame() {
-  // hide winning pop up
-  $('.winner').html("display", "none")
+//function startGame() {
+
   // give each space on gameboard a number 0-8 for gameBoard array
   gameBoard = Array.from(Array(9).keys());
-  // loop through each cell to display nothin, empty board
-  for (let i = 0; i < cells.length; i++) {
+  // loop through each cell to display nothing, empty board
+//  for (let i = 0; i < cells.length; i++) {
   // each cell has text of empty parenthesis
-    cells[i].innerText = ''
-  // add eventlistener to each square that has been clicked
-    cells[i].addEventListener("click", cellClick)
+  //  cells[i].innerText = ''
 
-  }
-}
+  // add eventlistener to each square that has been clicked
+  //  cells[i].addEventListener('click', cellClick)
+
+    // hide winning pop up
+    $('.winner').html("display", "none")
+
+  //}
+//}
 
 // player === 0 ? player === 1 : player === 0
 
 // display x or o when space on gameboard is clicked
 function cellClick(cell) {
 
-  playerTurn(cell.target.id, player)
+//  playerTurn(cell.target.id, player)
   // playerTurn(cell.target.id, player)
 
 }
 
 
 // display x or o when space on gameboard is clicked
-function playerTurn(cellId) {
+//function playerTurn(cellId) {
 
-  gameBoard[cellId] = player
-    if(player === 0) {
-      document.getElementById(cellId).innerHTML = "X"
-      player = 1
-    } else if (player === 1) {
-       document.getElementById(cellId).innerHTML = "O"
-      player = 0
-     }
-  }
+//  gameBoard[cellId] = player
+//    if(player === 0) {
+  //    $('#' + cellId).text('X')
+  //    player = 1
+  //  } else if (player === 1) {
+    //   document.getElementById(cellId).innerHTML = "O"
+  //    player = 0
+    // }
+  //}
 
 
 
@@ -89,11 +111,15 @@ function playerTurn(cellId) {
 
 // check if player won game using winning combos
 
+
+
 function checkWinner () {
+  for (let i = 0; i < cells.length; i++) {
+        if (cells[i] !== "") {
 
-  winners.forEach()
+        }
 
-
+  }
 
 }
 
